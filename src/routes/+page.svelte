5c8b1352006c7945c340c50cedddcc2e1e1d5eea
0xMarius <tech@marmakas.lt>
2023-05-04 00:26:05 +0300

    more content, some code refactoring

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,13 +40,13 @@
             <AnimatedHeadline texts={['Hello, I am ', 'Mijn naam is ', 'Sveiki, aš ', '哈喽，我是 ']}/>
              Marius.<Blinker class="flex"/>
         </h1>
-        <p transition:fly="{{ x: 200, duration: 2000 }}" class="pt-8 lg:pl-[3.7rem] text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <mark>Ut optime, secundum naturam affectum esse possit.</mark> <a href="http://loripsum.net/" target="_blank">Hanc quoque iucunditatem, si vis, transfer in animum;</a> <i>Quod equidem non reprehendo;</i> <a href="http://loripsum.net/" target="_blank">Duo Reges: constructio interrete.</a> Nec vero sum nescius esse utilitatem in historia, non modo voluptatem. Inde sermone vario sex illa a Dipylo stadia confecimus. Cum autem in quo sapienter dicimus, id a primo rectissime dicitur. </p>
-        <img class="pt-8 lg:pl-8 drop-shadow-[0rem_9.3rem_0px_rgba(0,0,0,0.5)]" src="../../plane.gif"/>
+        <p class="py-8 lg:pl-[3.7rem] text-justify"><mark>Welcome to my portfolio!</mark> As an artist turned developer, I'm passionate about creating 3D models, digital and acrylic art. My work is a reflection of my journey and showcases my skills in both design and technology. Take a look around and see how I blend creativity with technical expertise to produce unique and engaging visual experiences.</p>
+        <img class="lg:pl-8 drop-shadow-[0rem_10.8rem_0px_rgba(0,0,0,0.5)]" src="../../plane.gif"/>
     </div>
     <div class="flex items-center justify-center">
         <span class="inline-flex pt-64 sm:pt-14 w-4/5">
             <Printer />
-            <iconify-icon class="mt-58 text-4xl absolute" icon="ic:twotone-print"></iconify-icon>
+            <!-- <iconify-icon class="mt-58 text-4xl absolute" icon="ic:twotone-print"></iconify-icon> -->
         </span>
         <svg id="paper" class="sm:mt-[40.2rem] h-1/2 w-[13.2rem] sm:w-[24.1rem] inline-flex bg-[#f5f5f4] absolute">
             <rect />
@@ -64,15 +64,15 @@
 <div class="bg-stone-200 grid grid-cols1 lg:grid-cols-2">
     <div class="bg-[#d6d3d1]">
         <h1 class="pt-8 pb-12 text-center font-sans uppercase font-bold text-5xl">3D modeling</h1>
-        <p class="pb-8 lg:pl-[3.7rem] text-justify pb-8 font-serif">As someone who has used Blender for creating 3D models, I recommend it as a versatile and powerful tool. It offers a wide range of features and tools, along with an intuitive interface that makes it easy to get started. What I appreciate most about Blender is that it provides a lot of freedom for experimentation and customization via its node system, which allowed me to discover my own style and approach to 3D modeling. So, if you're interested in exploring the world of 3D modeling, I highly suggest giving Blender a try. It's a fantastic tool that can help you unlock your creativity and unleash your imagination.</p>
+        <p class="pb-8 lg:pl-[3.7rem] text-justify pb-8 font-serif">As someone who has used <mark>Blender for creating 3D models</mark>, I recommend it as a versatile and powerful tool. It offers a wide range of features and tools, along with an <mark>intuitive interface that makes it easy to get started.</mark> What I appreciate most about Blender is that it provides a lot of freedom for experimentation and customization via its <mark>extensive node system</mark>, which allowed me to discover my own style and approach to 3D modeling. So, if you're interested in exploring the world of 3D modeling, I highly suggest giving Blender a try. It's a fantastic tool that can help you unlock your creativity and unleash your imagination.</p>
     </div>
     <div class="bg-[#d6d3d1]"></div>
     <div class="bg-[#a8a29e]">
         <img src="../../ghost.gif" alt="ghost"/>
     </div>
-    <div class="py-8 bg-[#a8a29e]">
+    <div class="py-8 lg:px-[3.7rem] bg-[#a8a29e]">
         <h2 class="py-8 text-center font-sans font-bold text-5xl">A (very!) scary ghost</h2>
-        <p class="lg:pr-[3.7rem] text-justify font-serif">I came across an artist's tweet on Twitter featuring a lovely painting of a charming little ghost. It was so captivating that I felt inspired to recreate it in 3D. Using my 3D modeling skills, I created a cute and whimsical ghost, and to make it stand out, I added a glowing effect. The tricky part was getting the lighting juuuust right. It was a fun and fulfilling project that showcased the power of artistic inspiration.</p>
+        <p class="text-justify font-serif"><mark>I came across an artist's tweet on Twitter featuring a lovely painting of a charming little ghost.</mark> It was so captivating that I felt inspired to recreate it in 3D. Using my 3D modeling skills, I created a cute and whimsical ghost, and to make it stand out, I added a glowing effect. <mark>The tricky part was getting the lighting juuuust right.</mark> It was a fun and fulfilling project that showcased the power of artistic inspiration.</p>
     </div>
     <div class="bg-[#a8a29e]">
         <img src="../../ghost-shade.png" alt="ghost shaded"/>
@@ -80,9 +80,9 @@
     <div class="bg-[#a8a29e]">
         <img src="../../ghost-wire.png" alt="ghost wired"/>
     </div>
-    <div class="py-8 bg-[#78716c]">
+    <div class="lg:px-[3.7rem] py-8 bg-[#78716c]">
         <h2 class="py-8 text-center font-sans font-bold text-5xl">Various materials</h2>
-        <p class="lg:pl-[3.7rem] text-justify font-serif">In my quest to learn how to create different materials using Blender's node system, I decided to take on a challenging project: a 3D model of a cable that showcased the different layers of insulation. I worked my way from the outermost layer of rubber to the innermost layer of some kind of gel, experimenting with different node combinations and textures to create each layer's unique appearance. The result was a visually stunning 3D model that showcased the power of Blender's node system and my newfound knowledge of creating realistic materials.</p>
+        <p class="text-justify font-serif">In my quest to learn how to create different materials using Blender's node system, I decided to take on a challenging project: <mark>a 3D model of a cable that showcased the different layers of insulation. I worked my way from the outermost layer of rubber to the innermost layer of some kind of gel </mark>, experimenting with different node combinations and textures to create each layer's unique appearance. The result was a visually stunning 3D model that showcased the power of Blender's node system and my <mark>newfound knowledge of creating realistic materials.</mark></p>
     </div>
     <div class="bg-[#78716c]">
         <img src="../../cable.png" alt="cable"/>
@@ -90,22 +90,26 @@
     <div></div>
 </div>
 <div class="bg-stone-200 grid grid-cols-1">
-    <div  class="py-12 bg-[#d6d3d1]">
+    <div  class="lg:px-[7.4rem] py-12 bg-[#d6d3d1]">
         <h1 class="text-center font-sans uppercase font-bold text-5xl">Acrylic paintings</h1>
-        <p class="lg:pl-[7.4rem] lg:pr-[7.4rem] text-center font-serif">What I love most about acrylic paint is how fast it dries, which means that you can work quickly and build up layers to create texture and depth. I also appreciate how easy it is to mix and blend colors to achieve exactly the hue and shade you want. Acrylic paint an be used on so many surfaces, from canvas to wood to paper, which makes it a great choice for a variety of projects. Plus, the clean-up is a breeze, which means more time for painting and less time worrying about the mess.</p>
+        <p class="text-center font-serif">What I love most about acrylic paint is how fast it dries, which means that you can work quickly and build up layers to create texture and depth. I also appreciate how easy it is to mix and blend colors to achieve exactly the hue and shade you want. Acrylic paint an be used on so many surfaces, from canvas to wood to paper, which makes it a great choice for a variety of projects. Plus, the clean-up is a breeze, which means more time for painting and less time worrying about the mess.</p>
     </div>
 </div>
 <div class="bg-stone-200 grid grid-cols1 lg:grid-cols-2">
     <div class="flex justify-center bg-[#a8a29e]">
         <img class="w-[32rem]" src="../../mengmeng.png" alt="a gift"/>
     </div>
-    <div class="py-8 bg-[#a8a29e]">
+    <div class="lg:px-[3.7rem] py-8 bg-[#a8a29e]">
         <h2 class="py-8 font-sans font-bold text-5xl">A gift to 萌萌</h2>
-        <p class="lg:pr-[3.7rem] text-justify font-serif">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <mark>Ut optime, secundum naturam affectum esse possit.</mark> <a href="http://loripsum.net/" target="_blank">Hanc quoque iucunditatem, si vis, transfer in animum;</a> <i>Quod equidem non reprehendo;</i> <a href="http://loripsum.net/" target="_blank">Duo Reges: constructio interrete.</a> Nec vero sum nescius esse utilitatem in historia, non modo voluptatem. Inde sermone vario sex illa a Dipylo stadia confecimus. Cum autem in quo sapienter dicimus, id a primo rectissime dicitur. </p>
+        <p class=" text-justify font-serif">For my dear girlfriend 萌萌, an acrylic painting of her sitting amidst a colorful meadow on our weekend walk at the Park Frankendael. <mark>The painting features a fish-eye perspective, accentuating the vibrant colors of the sky and the ever so delicate flowers.</mark> A treasured memory of our cherished moments together.</p>
     </div>
-    <div class="py-8 bg-[#78716c]">
+    <div class="lg:pl-[3.7rem] py-8 bg-[#78716c]">
         <h2 class="py-8 text-center font-sans font-bold text-5xl">The emblem of class 1B</h2>
-        <p class="lg:pl-[3.7rem] text-justify font-serif"></p>
+        <p class="text-justify font-serif">The emblem of class 1B was a challenging project that required my expertise in both traditional and digital art. I began by painting a portrait of a majestic tiger using acrylics on a flat sheet of A3 size paper. The painting was then <mark>captured using my old phone, which resulted in a low-quality image.</mark> But that didn't stop me from bringing the painting to life digitally. <mark>I used various image processing techniques, including upscaling with AI</mark> and carefully enhancing the details to create a stunning digital painting that captures the essence of the original artwork. The end result was a beautiful emblem that represents the spirit of class 1B with pride and elegance.</p>
+        <div class="py-8 grid grid-cols1 lg:grid-cols-2">
+            <img src="tiger-og.jpg" alt="The original painting of a tiger"/>
+            <img src="tiger-edited.png" alt="Color adjusted version of the painting"/>
+        </div>
     </div>
     <div class="bg-[#78716c]">
         <img class="drop-shadow-[5rem_5rem_0px_rgba(0,0,0,1)]" src="../../tiger.png" alt="tiger">
