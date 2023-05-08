6d1654069c5c47474ec58c4689ea76a0c72011fe
0xMarius <tech@marmakas.lt>
2023-05-09 01:21:54 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      lineHeight: {
-        'extra-tight': '1.2',
-      },
-    },
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			lineHeight: {
+				'extra-tight': '1.2'
+			}
+		}
+	},
+	plugins: []
+};
